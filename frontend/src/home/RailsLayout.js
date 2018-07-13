6dcedd9597cb60c3a9ff7ce2abe0748e0fec4696
Joel Tio <joeltiojinhon@gmail.com>
2018-07-13 21:23:43 +0800

    Update TripleLayout to RailsLayout

--- frontend/src/home/RailsLayout.js
+++ frontend/src/home/RailsLayout.js
@@ -1,7 +1,9 @@
 import React from "react";
 import invariant from "invariant";
 
-export default class TripleLayout extends React.Component {
+import "./railsLayout.css";
+
+export default class RailsLayout extends React.Component {
     componentWillMount() {
         invariant(this.props.children.length == 3,
                   "TripleLayout should have 3 child elements.");
@@ -10,13 +12,13 @@ export default class TripleLayout extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col-md">
+                <div className="col-md-3" styleName="column">
                     {this.props.children[0]}
                 </div>
-                <div className="col-md">
+                <div className="col-md-6" styleName="column">
                     {this.props.children[1]}
                 </div>
-                <div className="col-md">
+                <div className="col-md-3" styleName="column">
                     {this.props.children[2]}
                 </div>
             </div>
