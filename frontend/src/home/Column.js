296b3af78e24b50c9a8fe6383807045b446580e9
Joel Tio <joeltiojinhon@gmail.com>
2018-07-13 23:15:16 +0800

    Add Column Component

---
+++ frontend/src/home/Column.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./column.css";
+
+export default class Column extends React.Component {
+    render() {
+        // If there are no elements, return an empty div
+        if (this.props.children === undefined) {
+            return <div></div>;
+        }
+
+        // Store all the elements into an array
+        var children = [];
+        // If there is only one element, that element is returned instead of an
+        // array.
+        if (this.props.children.length === undefined) {
+            children.push(this.props.children);
+        } else {
+            children = this.props.children;
+        }
+
+        var rows = [];
+        for (var i=0; i<children.length; i++) {
+            // Add border styles between the rows
+            var style = "item-row";
+            if (i != children.length && children.length != 1) {
+                style += " border-bottom";
+            }
+
+            rows.push(
+                <div key={i} className="row" styleName={style}>
+                    {children[i]}
+                </div>
+            );
+        }
+
+        return (
+            <div>{rows}</div>
+        );
+    }
+}
