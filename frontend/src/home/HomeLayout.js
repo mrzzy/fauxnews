8dc6af420724d693d06ee1515eea4f07f07453cb
Joel Tio <joeltiojinhon@gmail.com>
2018-07-14 05:39:32 +0800

    Add HomeLayout and ajax

---
+++ frontend/src/home/HomeLayout.js
@@ -0,0 +1,46 @@
+import React from "react";
+import axios from "axios"
+
+import NewsBlockLayout from "../newsblock/NewsBlockLayout";
+import HeaderLayout from "../header/HeaderLayout";
+
+import "./homeLayout.css";
+import RailsNewsLayout from "../news_columns/RailsNewsLayout";
+
+export default class HomeLayout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { newLayouts: [] };
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <HeaderLayout />
+                <RailsNewsLayout>
+                    {this.state.newLayouts}
+                </RailsNewsLayout>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        axios.get("http://127.0.0.1:8000/api/articles/", {
+                headers: {
+                    'Access-Control-Request-Method': 'GET',
+                    "Access-Control-Request-Headers": "Content-Type"
+                }
+            }
+        ).then((response) => {
+            var layouts = [];
+            for (var i=0; i<response.data.length; i++) {
+                var d = response.data[i];
+                layouts.push(
+                    <NewsBlockLayout key={i} thumbnailUrl={d["thumbnail_url"]}
+                        timestamp={d["datePublished"]} headline={d["title"]} />
+                );
+            }
+            this.setState({ newLayouts: layouts });
+        });
+    }
+}
