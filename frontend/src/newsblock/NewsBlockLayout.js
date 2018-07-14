b39ae3a75ad6d1ad65dc0e5aa4b345bfdb00cd2a
Joel Tio <joeltiojinhon@gmail.com>
2018-07-14 11:58:01 +0800

    Add news block hover effects

--- frontend/src/newsblock/NewsBlockLayout.js
+++ frontend/src/newsblock/NewsBlockLayout.js
@@ -7,17 +7,37 @@ import NewsExcerpt from "./NewsExcerpt";
 import "./newsBlockLayout.css";
 
 export default class NewsBlockLayout extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { mouseHover: false };
+    }
+
+    handleOnMouseEnter() {
+        this.setState({ mouseHover: true });
+    }
+
+    handleOnMouseLeave() {
+        this.setState({ mouseHover: false })
+    }
+
     render() {
         var thumbnail = null;
         if (this.props.thumbnailUrl) {
             thumbnail = <NewsThumbnail src={this.props.thumbnailUrl} />;
         }
         return (
-            <div styleName="news-block">
-                {thumbnail}
-                <NewsTimestamp>{this.props.timestamp}</NewsTimestamp>
-                <NewsHeadline>{this.props.headline}</NewsHeadline>
-                <NewsExcerpt>{this.props.excerpt}</NewsExcerpt>
+            <div styleName="news-block"
+                onMouseEnter={this.handleOnMouseEnter.bind(this)}
+                onMouseLeave={this.handleOnMouseLeave.bind(this)}>
+                <a styleName="header-anchor" href={this.props.url}>
+                    {thumbnail}
+                    <NewsTimestamp hovering={this.state.mouseHover}>{this.props.timestamp}</NewsTimestamp>
+                    <NewsHeadline hovering={this.state.mouseHover}>{this.props.headline}</NewsHeadline>
+                </a>
+                <a styleName="excerpt-anchor" href={this.props.url}>
+                    <NewsExcerpt hovering={this.state.mouseHover}>{this.props.excerpt}</NewsExcerpt>
+                </a>
             </div>
         );
     }
--- frontend/src/newsblock/NewsExcerpt.js
+++ frontend/src/newsblock/NewsExcerpt.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import "./newsExcerpt.css";
+
 export default class NewsExcerpt extends React.Component {
     render() {
+        var styles = "excerpt";
+        if (this.props.hovering) {
+            styles += " hovering";
+        }
         return (
-            <p>{this.props.children}</p>
+            <p styleName={styles}>{this.props.children}</p>
         );
     }
 }
