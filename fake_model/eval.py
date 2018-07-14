934e63f69a3e4310c06c3a5948c6448cb036de53
Zhu Zhan Yan <program.nom@gmail.com>
2018-07-14 08:48:59 +0800

    Update news evaluation code.

--- fake_model/eval.py
+++ fake_model/eval.py
@@ -9,8 +9,16 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import pandas as pd
+import glob
+import pickle 
+import sys
+from urllib.parse import unquote
 
-URL_SRC= ""
+from multiprocessing.pool import Pool
+from multiprocessing import cpu_count
+
+URL_SRC = "../scraper/facebook_urls.csv"
+ 
 
 # load model with timestamp in filename
 def load_model(name_prefix="sklearn_linearsvc_model", suffix=".pickle"):
@@ -23,19 +31,26 @@ def load_model(name_prefix="sklearn_linearsvc_model", suffix=".pickle"):
     with open(fname, "rb") as f:
         return pickle.load(f)
 
-# Retrieves and parse the contents of urls, returing 2 list with the following
+# convert those pesky facebook urls to normal urls
+def convert_urls(urls):
+    no_fb_urls = [ url.replace("https://l.facebook.com/l.php?u=","") for url in urls ]
+    unencoded_urls = [ unquote(url) for url in no_fb_urls ]
+    
+    return unencoded_urls
+    
+    
+# Retrieves and parse the content one url, tuple of the following
 # values:
 # titles - title of the article
 # text - text of the article 
 # img url - image url of the article
-def parse_urls(urls):
-    titles = []
-    texts = []
-    img_urls = []
-    for url in urls:
+def parse_url(url):
+    try:
         # Retrieve contents of url
-        r = requests.get(url)
-        soup =  BeautifulSoup(r.text, "html.parser")
+        # Lie that we a browser not a damned web crawer
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "html.parser")
         
         # Parse contents of url
         text = ""
@@ -44,13 +59,17 @@ def parse_urls(urls):
         
             
         imgs = soup.find_all("img")
-        if len(imgs) > 0:
-            img_urls.append(imgs[0].get("src"))
-        titles.append(soup.title.string)
-        texts.append(text)
+        img_url = None if len(imgs) <= 0 else imgs[0].get("src")
+
         
+        title = None if soup.title == None else soup.title.string
+        
+        print("Parsed {}...".format(url))
     
-    return titles, texts, img_urls
+        return title, text, img_url
+    except:
+        return None, None, None
+        
     
 # Predict the legitmacy for the given news article specfied by the title and text
 # returns a list of prediction results, with 1 makring fake news
@@ -69,14 +88,59 @@ def evaluate_legitmacy(titles, texts):
 if __name__ == "__main__":
     # Read the data
     df = pd.read_csv(URL_SRC)
-    urls.loc[:,"url"]
- 
-    # Add parsed data to dataframe
-    titles, texts, img_urls = parse_urls(urls)
-    df["title"] = titles
-    df["text"] = texts
-    df["img_url"] = img_urls
-    df["fake"] = evaluate_legitmacy(titles, texts)
-
+    raw_urls = convert_urls(df.loc[:,"url"])
+    raw_authors = df.loc[:,"author"]
+    raw_timestamps = df.loc[:,"date"]
     
+    # Filter out duplicate urls
+    duplicate = []
+    url_set = set()
+    for url in raw_urls:
+        if not url in url_set:
+            duplicate.append(False)
+            url_set.add(url)
+        else:
+            duplicate.append(True)
+        
+    
+    assert len(duplicate) == len(raw_urls)
+    
+    filtered_urls = [ raw_urls[i] for i in range(len(raw_urls)) if not duplicate[i] ]
+    filtered_authors = [ raw_authors[i] for i in range(len(raw_urls)) if not duplicate[i] ]
+    filtered_timestamps = [ raw_timestamps[i] for i in range(len(raw_urls)) if not duplicate[i] ]
+    
+    # Concurrent retrieval of news article data
+    parsed_df = list(map(parse_url, filtered_urls))
+    
+    # Filter out urls with missing or deleted pages
+    urls = [] 
+    titles = []
+    texts = []
+    img_urls = []
+    timestamps = []
+    authors = []
+    
+    title_set = set()
+
+    for i, url in enumerate(filtered_urls):
+        entry = parsed_df[i]
+
+        if entry[0] != None and not "404" in entry[0].lower() and not "not found" in entry[0].lower() and entry[1] != None and not entry[0] in title_set:
+            urls.append(url)
+            titles.append(entry[0])
+            texts.append(entry[1])
+            img_urls.append(entry[2])
+            timestamps.append(filtered_timestamps[i])
+            authors.append(filtered_authors[i])
+            title_set.add(entry[0])
+            
+        
+    assert len(urls) == len(titles) == len(texts) == len(img_urls) == len(timestamps) == len(authors)
+
+    # Rank news as fake or real
+    legitmacy_ratings = evaluate_legitmacy(titles, texts)
+
+    df = pd.DataFrame(data={"title": titles, "fake": legitmacy_ratings, "url": urls, "text": texts, "img_url": img_urls, "timestamp": timestamps, "author": authors})
+    df = df[df.fake == 1]
+    print(df)
     df.to_csv("../backend/menu.csv")
